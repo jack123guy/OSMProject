c212c5ccd573c334ae4eecaf8eb5b2224f029be4
jack123guy <jack123guy@gmail.com>
2017-06-13 15:36:23 +0800

    Fix most of the Problem

--- app/src/main/java/com/example/alternative_admin/osmproject/MainActivity.java
+++ app/src/main/java/com/example/alternative_admin/osmproject/MainActivity.java
@@ -1,13 +1,108 @@
 package com.example.alternative_admin.osmproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+
+public class MainActivity extends Activity {
+
+    private Button NextPage;
+    private Button GetInfo;
+    private TextView Info;
+    String title;
+    String url = "http://www.cwb.gov.tw/V7/forecast/taiwan/Tainan_City.htm";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_map);
+
+        NextPage = (Button) findViewById(R.id.next);
+        GetInfo = (Button) findViewById(R.id.GetInfo);
+        Info = (TextView) findViewById(R.id.WebInfo);
+        NextPage.setOnClickListener(new button_set());
+        GetInfo.setOnClickListener(new button_set());
+
     }
+
+    private class button_set implements OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            if(v.getId() == R.id.next){
+                // TODO Auto-generated method stub
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,Map.class);
+                startActivity(intent);
+            }else if(v.getId() == R.id.GetInfo){
+                //Info.setText("New Info From WebPage");
+                new Thread(runnable).start();
+            }
+
+        }
+
+    }
+
+
+
+    Runnable runnable = new Runnable(){
+        @Override
+        public void run(){
+            try{
+                Document doc = Jsoup.connect(url).get();
+                Elements weather = doc.select("td");
+                title = weather.get(0).text();
+            }catch(IOException e){
+                //TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            handler.sendEmptyMessage(0);
+        }
+    };
+
+    @SuppressLint("HandlerLeak")
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg){
+            super.handleMessage(msg);
+            Info.setText(title);
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.map, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
--- app/src/main/java/com/example/alternative_admin/osmproject/Map.java
+++ app/src/main/java/com/example/alternative_admin/osmproject/Map.java
@@ -4,111 +4,208 @@ package com.example.alternative_admin.osmproject;
  * Created by alternative-admin on 2017/6/12.
  */
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Intent;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+import org.osmdroid.DefaultResourceProxyImpl;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapController;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlay;
+import org.osmdroid.views.overlay.OverlayItem;
+import org.osmdroid.views.overlay.ScaleBarOverlay;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 import java.io.IOException;
-
+import java.util.ArrayList;
 
 public class Map extends Activity {
+    private MapView mapview;
+    private MapController mcontrol;
+    private ScaleBarOverlay scale;
+    public LocationManager locationManager = null;
+    private ItemizedOverlay<OverlayItem> iconOverlay;
 
+    private Button back;
+    private Button Info;
 
-    private Button NextPage;
-    private Button GetInfo;
-    private TextView Info;
-    String title;
+    private final Point mpoint = new Point();
+
+    protected final Paint mpaint = new Paint();
+
+    double lat = 22.998519;
+    double lng = 120.220106;
+    DefaultResourceProxyImpl defaultResourceProxyImpl;
     String url = "http://www.cwb.gov.tw/V7/forecast/taiwan/Tainan_City.htm";
-
+    String info;
+    Location location;
+    GeoPoint myLocation;
+    //private MyLocationNewOverlay mMyLocationOverlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
+        setContentView(R.layout.activity_main);
+        initialmap();
 
-        NextPage = (Button) findViewById(R.id.next);
-        GetInfo = (Button) findViewById(R.id.GetInfo);
-        Info = (TextView) findViewById(R.id.WebInfo);
-        NextPage.setOnClickListener(new button_set());
-        GetInfo.setOnClickListener(new button_set());
-
-    }
-
-    private class button_set implements OnClickListener {
-
-        @Override
-        public void onClick(View v) {
-            if (v.getId() == R.id.next) {
-                // TODO Auto-generated method stub
-                Intent intent = new Intent();
-                intent.setClass(Map.this, MainActivity.class);
-                startActivity(intent);
-            } else if (v.getId() == R.id.GetInfo) {
-                //Info.setText("New Info From WebPage");
-                new Thread(runnable).start();
-            }
-
+        //Get Location
+        location = getMyLocation();
+        if(location == null){
+            //Testing Locaition on the emulator
+            myLocation = new GeoPoint(lat,lng);
+            MyLocationNewOverlay myLocationOverlay = new MyLocationNewOverlay(this, mapview);
+            mcontrol.setCenter(myLocation);
+            mapview.getOverlays().add(myLocationOverlay);
+            Toast.makeText(getApplicationContext(), "Test location in emulator", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            myLocation = new GeoPoint(location);
+            MyLocationNewOverlay myLocationOverlay = new MyLocationNewOverlay(this, mapview);
+            mcontrol.setCenter(myLocation);
+            mapview.getOverlays().add(myLocationOverlay);
         }
 
+        AddMarker();
+        draw(null, mapview, true);
+        //Add Scale Bar
+        scale = new ScaleBarOverlay(this);
+        mapview.getOverlays().add(scale);
+
+        //Add Button
+        back = (Button) findViewById(R.id.back);
+        Info = (Button) findViewById(R.id.Info);
+        back.setOnClickListener(button_set);
+        Info.setOnClickListener(button_set);
+
+
+    }
+    public void draw(final Canvas c, MapView map, final boolean shadow){
+        if(shadow) return;
+        final MapView.Projection pj = map.getProjection();
+        pj.toMapPixels(myLocation, mpoint);
+        c.drawText("Tainan Weather",mpoint.x,mpoint.y,this.mpaint);
     }
 
 
-    Runnable runnable = new Runnable() {
+
+    //Set MapView
+    public void initialmap(){
+        mapview = (MapView) this.findViewById(R.id.mapview);
+        mapview.setTileSource(TileSourceFactory.MAPQUESTOSM);
+        mapview.setBuiltInZoomControls(true);
+        mapview.setMultiTouchControls(true);
+        mapview.setClickable(true);
+        mcontrol = (MapController) this.mapview.getController();
+        mcontrol.setZoom(15);
+    }
+
+    private View.OnClickListener button_set = new View.OnClickListener(){
+        public void onClick(View v){
+            //TODO Auto-generated method stub
+            switch(v.getId()){
+                case R.id.back:
+                    mcontrol.setCenter(myLocation);
+                    break;
+                case R.id.Info:
+                    new Thread(runnable).start();
+                    Toast.makeText(getApplicationContext(), "Work", Toast.LENGTH_SHORT).show();
+                    break;
+
+
+            }
+        }
+    };
+	/*
+	public void Addtext(){
+		//Create Overlay for marker
+				OverlayItem myOverlayItem =
+						new OverlayItem("Here","Current Position", myLocation);
+				String marker =
+						"lan";
+				myOverlayItem.setMarker(marker);
+
+				ArrayList<OverlayItem> overlayArray =
+						new ArrayList<OverlayItem>();
+				overlayArray.add(myOverlayItem);
+
+				defaultResourceProxyImpl = new DefaultResourceProxyImpl(this);
+				this.iconOverlay = new ItemizedIconOverlay<OverlayItem>(overlayArray, marker,null, defaultResourceProxyImpl);
+				mapview.getOverlays().add(iconOverlay);
+
+	}
+	 */
+
+
+    public void AddMarker(){
+        //Create Overlay for marker
+        OverlayItem myOverlayItem =
+                new OverlayItem("Here","Current Position", myLocation);
+        Drawable marker =
+                ResourcesCompat.getDrawable(getResources(),R.drawable.markpin,null);
+        myOverlayItem.setMarker(marker);
+
+        ArrayList<OverlayItem> overlayArray =
+                new ArrayList<OverlayItem>();
+        overlayArray.add(myOverlayItem);
+
+        defaultResourceProxyImpl = new DefaultResourceProxyImpl(this);
+        this.iconOverlay = new ItemizedIconOverlay<OverlayItem>(overlayArray, marker,null, defaultResourceProxyImpl);
+        mapview.getOverlays().add(iconOverlay);
+
+    }
+
+    Runnable runnable = new Runnable(){
         @Override
-        public void run() {
-            try {
+        public void run(){
+            try{
                 Document doc = Jsoup.connect(url).get();
                 Elements weather = doc.select("td");
-                title = weather.get(0).text();
-            } catch (IOException e) {
+                info = weather.get(0).text();
+            }catch(IOException e){
                 //TODO Auto-generated catch block
                 e.printStackTrace();
             }
-            handler.sendEmptyMessage(0);
+            //handler.sendEmptyMessage(0);
         }
     };
+	/*
+	@SuppressLint("HandlerLeak")
+	Handler handler = new Handler(){
+		@Override
+		public void handleMessage(Message msg){
+			super.handleMessage(msg);
+			Info.setText(info);
+		}
+	};
+	*/
 
-    @SuppressLint("HandlerLeak")
-    Handler handler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            Info.setText(title);
+    Location getMyLocation(){
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        if ( ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED ){
+            ActivityCompat.requestPermissions( this, new String[] {  android.Manifest.permission.ACCESS_COARSE_LOCATION  },
+                    MY_PERMISSION_ACCESS_COARSE_LOCATION);
         }
-    };
+        return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.map, menu);
-        return true;
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-
 }
 
